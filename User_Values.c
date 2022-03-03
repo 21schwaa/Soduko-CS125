609c8bff1a255e71bbe7c25104ac358900b54640
Adam Schwartz <schwaa12@my.erau.edu>
2022-03-03 13:43:54 -0700

    Add files via upload

---
+++ User_Values.c
@@ -0,0 +1,26 @@
+#include <stdiio.h>
+
+int main(){
+
+char userInputRow, userInputCol;
+
+printf("Where would you like to Input?\n");
+printf("Please enter a ROW:A-I:");
+scanf("%c", &userInputRow);
+printf("Please enter a COL:1-9:");
+scnaf("%c", &userInputCol);
+printf("What Number would you like to enter?");
+scanf("%d", entryNumber);
+	
+if (userInputRow == 'A'){
+	if (userInputCol == 1){
+		board[0][1]=entryNumber;
+	}
+}
+	
+	
+	
+	
+	
+	
+}
