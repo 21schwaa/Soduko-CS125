d8d3053b9cd4fc12b3a3e50b7a61e2baff5d7548
Adam Schwartz <schwaa12@my.erau.edu>
2022-03-10 13:36:28 -0700

    Update User_Values.c

--- User_Values.c
+++ User_Values.c
@@ -17,7 +17,91 @@ if (userInputRow == 'A'){
 	if (userInputCol == 1){
 		board[0][1]=entryNumber;
 	}
+	if (userInputCol == 2){
+		board[0][2]=entryNumber;
+	}
+	if (userInputCol == 3){
+		board[0][3]=entryNumber;
+	}
+	if (userInputCol == 4){
+		board[0][4]=entryNumber;
+	}
+	if (userInputCol == 5){
+		board[0][5]=entryNumber;
+	}
+	if (userInputCol == 6){
+		board[0][6]=entryNumber;
+	}
+	if (userInputCol == 7){
+		board[0][7]=entryNumber;
+	}
+	if (userInputCol == 8){
+		board[0][8]=entryNumber;
+	}
+	if (userInputCol == 9){
+		board[0][9]=entryNumber;
+	
 }
+if (userInputRow == 'B'){
+	if (userInputCol == 1){
+		board[1][1]=entryNumber;
+	}
+	if (userInputCol == 2){
+		board[1][2]=entryNumber;
+	}
+	if (userInputCol == 3){
+		board[1][3]=entryNumber;
+	}
+	if (userInputCol == 4){
+		board[1][4]=entryNumber;
+	}
+	if (userInputCol == 5){
+		board[1][5]=entryNumber;
+	}
+	if (userInputCol == 6){
+		board[1][6]=entryNumber;
+	}
+	if (userInputCol == 7){
+		board[1][7]=entryNumber;
+	}
+	if (userInputCol == 8){
+		board[1][8]=entryNumber;
+	}
+	if (userInputCol == 9){
+		board[1][9]=entryNumber;
+	
+}
+if (userInputRow == 'C'){
+	if (userInputCol == 1){
+		board[2][1]=entryNumber;
+	}
+	if (userInputCol == 2){
+		board[2][2]=entryNumber;
+	}
+	if (userInputCol == 3){
+		board[2][3]=entryNumber;
+	}
+	if (userInputCol == 4){
+		board[2][4]=entryNumber;
+	}
+	if (userInputCol == 5){
+		board[2][5]=entryNumber;
+	
+	if (userInputCol == 6){
+		board[2][6]=entryNumber;
+	}
+	if (userInputCol == 7){
+		board[2][7]=entryNumber;
+	}
+	if (userInputCol == 8){
+		board[2][8]=entryNumber;
+	}
+	if (userInputCol == 9){
+		board[2][9]=entryNumber;
+	
+}
+	
+
 	
 	
 	
